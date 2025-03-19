5f9cabdd31d0bb6c870372cd2d6454d897a5ebb5
dinaameil <dinaameil753@gmail.com>
2025-03-19 10:41:35 +0700

    Mengurutkan Data Mahasiswa Berdasarkan IPK - dengan data yang sudah tersedia

---
+++ MahasiswaDemo08.java
@@ -0,0 +1,23 @@
+public class MahasiswaDemo08 {
+    public static void main(String[] args) {
+        MahasiswaBerprestasi08 list = new MahasiswaBerprestasi08();
+        Mahasiswa08 m1 = new Mahasiswa08("123", "Zidan", "2A", 3.2);
+        Mahasiswa08 m2 = new Mahasiswa08("124", "Ayu", "2A", 3.5);
+        Mahasiswa08 m3 = new Mahasiswa08("125", "Sofi", "2A", 3.1);
+        Mahasiswa08 m4 = new Mahasiswa08("126", "Sita", "2A", 3.9);
+        Mahasiswa08 m5 = new Mahasiswa08("127", "Miki", "2A", 3.7);
+
+        list.tambah(m1);
+        list.tambah(m2);
+        list.tambah(m3);
+        list.tambah(m4);
+        list.tambah(m5);
+
+        System.out.println("Data Mahasiswa sebelum sorting: ");
+        list.tampil();
+
+        System.out.println("Data Mahasiswa setelah sorting berdasarkan IPK (DESC)");
+        list.bubbleSort();
+        list.tampil();
+    }
+}
